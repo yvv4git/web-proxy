dea0b40fe79c6ca54b57a8d362909dbf3c87d637
yvv4git <yvv4m@mail.ru>
2025-10-09 21:18:25 +0300

    Refactoring

--- internal/webproxy/webproxy.go
+++ internal/webproxy/webproxy.go
@@ -9,34 +9,50 @@ import (
 	"go.uber.org/zap"
 )
 
-type WebProxy struct {
-	log    *zap.Logger
-	webSrv *http.Server
+type Option func(*WebProxy)
+
+func WithAddr(addr string) Option {
+	return func(wp *WebProxy) {
+		wp.webSrv.Addr = addr
+	}
 }
 
-func NewWebProxy(log *zap.Logger) *WebProxy {
+func WithShutdownTimeout(timeout time.Duration) Option {
+	return func(wp *WebProxy) {
+		wp.shutdownTimeout = timeout
+	}
+}
+
+type WebProxy struct {
+	log             *zap.Logger
+	shutdownTimeout time.Duration
+	webSrv          *http.Server
+}
+
+func NewWebProxy(log *zap.Logger, opts ...Option) *WebProxy {
 	const (
 		defaultAddr = "127.0.0.1:8080"
 	)
 
 	proxy := goproxy.NewProxyHttpServer()
 
-	webSrv := &http.Server{
+	entity := &WebProxy{}
+	entity.log = log
+	entity.webSrv = &http.Server{
 		Addr:    defaultAddr,
 		Handler: proxy,
 	}
 
-	wp := &WebProxy{
-		log:    log,
-		webSrv: webSrv,
+	for _, opt := range opts {
+		opt(entity)
 	}
 
 	// For all standard requests
-	proxy.OnRequest().Do(goproxy.FuncReqHandler(wp.authMiddleware))
+	proxy.OnRequest().Do(goproxy.FuncReqHandler(entity.authMiddleware))
 
 	// For connect requests in https
 	proxy.OnRequest().HandleConnect(goproxy.FuncHttpsHandler(func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
-		_, resp := wp.authMiddleware(ctx.Req, ctx)
+		_, resp := entity.authMiddleware(ctx.Req, ctx)
 		if resp != nil {
 			ctx.Resp = resp
 			return goproxy.RejectConnect, host
@@ -45,12 +61,10 @@ func NewWebProxy(log *zap.Logger) *WebProxy {
 		return goproxy.OkConnect, host
 	}))
 
-	return wp
+	return entity
 }
 
 func (wp *WebProxy) Start(ctx context.Context) error {
-	const shutdownTimeout = 5 * time.Second // TODO: вынести в конфиг
-
 	go func() {
 		wp.log.Info("starting web proxy", zap.String("address", wp.webSrv.Addr))
 		if err := wp.webSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
@@ -62,7 +76,7 @@ func (wp *WebProxy) Start(ctx context.Context) error {
 
 	wp.log.Info("shutting down web proxy")
 
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), wp.shutdownTimeout)
 	defer cancel()
 
 	if err := wp.webSrv.Shutdown(shutdownCtx); err != nil {
