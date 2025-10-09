089cfb2999484c9588e2154d66772420b80c1a0c
yvv4git <yvv4m@mail.ru>
2025-10-09 22:00:52 +0300

    Refactoring

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/yvv4git/web-proxy
 go 1.24.2
 
 require (
-	github.com/davecgh/go-spew v1.1.1
 	github.com/elazarl/goproxy v1.7.2
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/spf13/cobra v1.10.1
@@ -13,6 +12,7 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- internal/webproxy/application.go
+++ internal/webproxy/application.go
@@ -7,7 +7,6 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/yvv4git/web-proxy/internal/config"
 	"github.com/yvv4git/web-proxy/internal/infra"
 	"go.uber.org/zap"
@@ -17,14 +16,12 @@ func RunWebProxy(configPath string) {
 	var cfg config.Config
 
 	if err := config.Load(configPath, &cfg); err != nil {
-		stdLog.Fatalf("Failed to load config: %v", err)
+		stdLog.Fatalf("failed to load config: %v", err)
 	}
-
-	spew.Dump(cfg)
 
 	log, err := infra.NewWithLogLevel(cfg.LogLevel)
 	if err != nil {
-		stdLog.Fatalf("Failed to create logger: %v", err)
+		stdLog.Fatalf("failed to create logger: %v", err)
 	}
 	defer log.Sync()
 
@@ -38,6 +35,6 @@ func RunWebProxy(configPath string) {
 	}
 
 	if err := webProxy.Start(ctx); err != nil {
-		log.Fatal("Failed to start web proxy: %v", zap.Error(err))
+		log.Fatal("failed to start web proxy: %v", zap.Error(err))
 	}
 }
